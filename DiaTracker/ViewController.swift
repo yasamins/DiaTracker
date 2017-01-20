090201c314da288d7c999b9f646aab6a451ebcde
yasamins <yasamin.salami@yahoo.com>
2017-01-20 19:19:43 +0200

    Initial Commit

---
+++ DiaTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiaTracker
+//
+//  Created by yasamin  on 20.1.2017.
+//  Copyright © 2017 yasamin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
